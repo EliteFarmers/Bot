92ffd84fa8ad0a2bbbb150a44de60c3985d7f624
Kaeso <edmier12@gmail.com>
2021-06-29 15:16:08 -0500

    Fixed help

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const { DataHandler } = require('../database.js');
 let { prefix } = require('../config.json');
 module.exports = {
@@ -7,51 +8,61 @@ module.exports = {
 	usage: '[command name]',
 	guildOnly: false,
 	async execute(message, args) {
-		message.channel.send('Help is broken, will fix later');
-		// 	const data = [];
-		// 	const { commands } = message.client;
-		// 	if (message.guild !== null) {
-		// 		newPrefix = await DataHandler.getPrefix(message.guild.id).then(obj => { return (obj !== null) ? obj.dataValues.prefix : prefix});
-		// 	} else {
-		// 		newPrefix = prefix;
-		// 	}
-		// 	//const helpMenu = new Discord.RichEmbed()
-		// 	if (!args.length) {
-		// 		//helpMenu.title('Here\'s a list of all the commands:')
+		const data = [];
+		const { commands } = message.client;
+		if (message.guild !== null) {
+			newPrefix = await DataHandler.getPrefix(message.guild.id).then(obj => { return (obj !== null) ? obj.dataValues.prefix : prefix});
+		} else {
+			newPrefix = prefix;
+		}
+		if (!args.length) {
+			helpMenu = getHelpEmbed();
+			return message.reply({ embeds: [helpMenu], allowedMentions: { repliedUser: false } });
+		}
+		if (args[0] !== undefined) {
+			const name = args[0].toLowerCase();
+			const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
-		// 		data.push('Here\'s a list of all the commands:');
-		// 		data.push(commands.map(command => command.name).join(', '));
-		// 		data.push(`\nYou can send \`${newPrefix ?? prefix}help [command name]\` to get info on a specific command!`);
-		// 		/*
-		// 		return message.author.send(data, { split: true })
-		// 			.then(() => {
-		// 				if (message.channel.type === 'dm') return;
-		// 				message.reply('I\'ve sent you a DM with all my commands!');
-		// 			})
-		// 			.catch(error => {
-		// 				console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-		// 				message.reply('It seems like I can\'t DM you!');
-		// 			});
-		// 		*/
-		// 	}
+			if (!command) {
+				let embed = getHelpEmbed()
+				return message.reply({ content: 'That\'s not a valid command! Here\'s the menu instead.', embeds: [embed], allowedMentions: { repliedUser: false }});
+			}
 
-		// 	if (args[0] !== undefined) {
-		// 		const name = args[0].toLowerCase();
-		// 		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+			let embed = new Discord.MessageEmbed()
+				.setColor('#03fc7b')
+				.setTitle(`Usage for ${command.name}`);
+			
+			let data = [];
+			data.push(`**Name:** ${command.name}`);
 
-		// 		if (!command) {
-		// 			return message.reply('that\'s not a valid command!');
-		// 		}
+			if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+			if (command.description) data.push(`**Description:** ${command.description}`);
+			if (command.usage) data.push(`**Usage:** ${newPrefix}${command.name} ${command.usage}`);
 
-		// 		data.push(`**Name:** ${command.name}`);
+			embed.fields.push({
+				name: 'Command Information',
+				value: `${data.join('\n')}`,
+				inline: false
+			})
 
-		// 		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-		// 		if (command.description) data.push(`**Description:** ${command.description}`);
-		// 		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+			return message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+		}
 
-		// 		//data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-		// 	}
-		// 	message.channel.send(data,);
-		// }
+		function getHelpEmbed() {
+			const helpMenu = new Discord.MessageEmbed().setColor('#03fc7b');
+
+			helpMenu.setTitle('Here\'s a list of all the commands:')
+
+			commands.forEach(command => {
+				helpMenu.fields.push({
+					name: `${newPrefix}${command.name} (${command.aliases.join(', ')})`,
+					value: `${command.description}\n${newPrefix}${command.name} ${command.usage}`,
+					inline: false
+				});
+			});
+
+			helpMenu.setFooter(`\nYou can send \`${newPrefix ?? prefix}help [command name]\` to get info on a specific command!`);
+			return helpMenu;
+		}
 	}
 }
