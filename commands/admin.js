0c89c01795565bfaf54327c54fe79300647a7955
ptlthg <edmier12@gmail.com>
2021-11-02 15:25:39 -0400

    Remove cached player when changing cheating status

--- commands/admin.js
+++ commands/admin.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const { Auth } = require('../auth.js');
+const { PlayerHandler } = require('../calc.js');
 const { superusers } = require('../config.json');
 const { DataHandler } = require('../database.js');
 
@@ -107,6 +108,9 @@ module.exports = {
 										await DataHandler.getPlayer(user.dataValues.uuid).then(user => {
 											collected.first().reply({ content: `${user.dataValues.ign} has been labled as ${toggle ? 'cheating.' : 'legit.'}` });
 										});
+										if (PlayerHandler.cachedPlayers.has(user.dataValues?.ign.toLowerCase())) {
+											PlayerHandler.cachedPlayers.delete(user.dataValues?.ign.toLowerCase());
+										}
 										throw new Error();
 									} else {
 										collected.first().reply({ content: 'Specify a valid uuid next time.' })
