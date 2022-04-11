9db0e47af9f6ecc29f442f132883caa11780f374
ptlthg <edmier12@gmail.com>
2022-04-11 09:43:13 -0400

    Good interaction handler

---
+++ classes/Util.ts
@@ -0,0 +1,50 @@
+import { Guild, GuildBasedChannel, Snowflake, ThreadChannelTypes } from "discord.js";
+import { client } from "../index";
+import { CommandAccess } from "./Command";
+
+export function isValidAccess(access: CommandAccess, type: 'DM' | 'GUILD_NEWS' | 'GUILD_TEXT' | ThreadChannelTypes): boolean {
+	if (access === 'ALL') return true;
+	// If access is direct, return true if type is also a DM, else false
+	if (access === 'DIRECT') return (type === 'DM');
+	// Access has to be GUILD at this point, so return true as long as the channel isn't a DM
+	return (type !== 'DM');
+}
+
+/**
+ * Given a GuildID or Guild object, finds the given channel
+ * 
+ * @param  {Snowflake|Guild} guild
+ * @param  {Snowflake} channelId
+ * @returns {Promise<GuildBasedChannel | undefined>}
+ */
+export async function FindChannel(guild: Snowflake | Guild, channelId: Snowflake): Promise<GuildBasedChannel | undefined> {
+	let guildObj = guild;
+
+	if (typeof guildObj === 'string') {
+		const fetchGuild = await FindGuild(guildObj);
+		if (fetchGuild) {
+			guildObj = fetchGuild;
+		} return undefined;
+	}
+	
+	if (guildObj.channels.cache.has(channelId)) {
+		return guildObj.channels.cache.get(channelId);
+	}
+	
+	const channel = await guildObj.channels.fetch(channelId);
+	return channel ?? undefined;
+}
+/**
+ * Finds a guild given an ID or undefined
+ * 
+ * @param  {Snowflake} guildId
+ * @returns {Promise<Guild | undefined>}
+ */
+export async function FindGuild(guildId: Snowflake): Promise<Guild | undefined> {
+	if (client.guilds.cache.has(guildId)) {
+		return client.guilds.cache.get(guildId);
+	}
+	
+	const guild = await client.guilds.fetch(guildId);
+	return guild ?? undefined;
+}
---
+++ events/interactionCreate.ts
@@ -0,0 +1,81 @@
+import { AutocompleteInteraction, ButtonInteraction, CommandInteraction, Interaction, Permissions, PermissionString } from "discord.js";
+import { commands } from "../index";
+import { Command, CommandType } from "../classes/Command";
+import { isValidAccess } from "../classes/Util";
+
+export default async function(interaction: Interaction) {
+	if (interaction.isCommand()) return OnCommandInteraction(interaction);
+	if (interaction.isButton()) return OnButtonInteraction(interaction);
+	if (interaction.isAutocomplete()) return OnAutocompleteInteraction(interaction);
+}
+
+async function OnCommandInteraction(interaction: CommandInteraction) {
+
+	const command = GetCommand(interaction.commandName, 'SLASH');
+	if (!command) return;
+
+	if (!HasPermsAndAccess(command, interaction)) return;
+
+	try {
+		command.execute(interaction);
+	} catch (error) {
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true }).catch(() => undefined);
+	}
+}
+
+async function OnButtonInteraction(interaction: ButtonInteraction) {
+	const args = interaction.customId.split('|');
+	const commandName = args[0];
+
+	const command = GetCommand(commandName, 'COMPONENT');
+	if (!command) return;
+
+	if (!HasPermsAndAccess(command, interaction)) return;
+
+	try {
+		command.execute(interaction);
+	} catch (error) {
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true }).catch();
+	}
+}
+
+async function OnAutocompleteInteraction(interaction: AutocompleteInteraction) {
+	const commandName = interaction.commandName;
+
+	if (!commands.has(commandName)) return;
+
+	const command: Command | undefined = commands.get(commandName);
+	if (!command || command.type !== 'AUTOCOMPLETE') return;
+
+	try {
+		command.execute(interaction);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+function GetCommand(name: string, type: CommandType): Command | undefined {
+	const command: Command | undefined = commands.get(name);
+	if (!command || command.type !== type) return undefined;
+	return command;
+}
+
+async function HasPermsAndAccess(command: Command, interaction: CommandInteraction | ButtonInteraction) {
+	if (interaction.channel && !isValidAccess(command.access, interaction.channel.type)) return false;
+
+	if (!interaction.guildId || !command.permissions) return true;
+
+	// Get user permissions
+	const perms = ((interaction.member?.permissions) as Readonly<Permissions>).toArray();
+	// Return if lacking one
+	if (!(command.permissions.every((perm) => perms.includes(perm as PermissionString)))) {
+		await interaction.reply({ 
+			content: 'You don\'t have the required permissions for this command.', 
+			allowedMentions: { repliedUser: true }, 
+			ephemeral: true 
+		});
+		return false;
+	}	
+
+	return true;
+}
