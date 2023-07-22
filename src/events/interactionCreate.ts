14d1ff01c789bbaa67e4549aa3587df4db332cb3
ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-07-22 03:38:38 -0400

    Update interactionCreate event

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,18 +1,18 @@
-import { AutocompleteInteraction, ButtonInteraction, CommandInteraction, GuildMember, Interaction } from 'discord.js';
+import { ButtonInteraction, CommandInteraction, GuildMember, Interaction } from 'discord.js';
 import { commands } from '../index';
 import { Command, CommandType } from '../classes/Command';
 import { HasRole, isValidAccess } from '../classes/Util';
-import DataHandler from '../classes/Database';
+import { FetchGuild } from 'api/elite';
 
 export default async function(interaction: Interaction) {
 	if (interaction.isCommand()) return OnCommandInteraction(interaction);
 	if (interaction.isButton()) return OnButtonInteraction(interaction);
-	if (interaction.isAutocomplete()) return OnAutocompleteInteraction(interaction);
+	//if (interaction.isAutocomplete()) return OnAutocompleteInteraction(interaction);
 }
 
 async function OnCommandInteraction(interaction: CommandInteraction) {
 
-	const command = GetCommand(interaction.commandName, 'SLASH');
+	const command = GetCommand(interaction.commandName, CommandType.Slash);
 	if (!command) return;
 
 	const hasPerms = await HasPermsAndAccess(command, interaction);
@@ -21,7 +21,10 @@ async function OnCommandInteraction(interaction: CommandInteraction) {
 	try {
 		command.execute(interaction);
 	} catch (error) {
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true }).catch(() => undefined);
+		await interaction.reply({ 
+			content: 'There was an error while executing this command!', 
+			ephemeral: true 
+		}).catch(() => undefined);
 	}
 }
 
@@ -29,7 +32,7 @@ async function OnButtonInteraction(interaction: ButtonInteraction) {
 	const args = interaction.customId.split('|');
 	const commandName = args[0];
 
-	const command = GetCommand(commandName, 'BUTTON');
+	const command = GetCommand(commandName, CommandType.Button);
 	if (!command) return;
 
 	const hasPerms = await HasPermsAndAccess(command, interaction);
@@ -42,54 +45,14 @@ async function OnButtonInteraction(interaction: ButtonInteraction) {
 	}
 }
 
-async function OnAutocompleteInteraction(interaction: AutocompleteInteraction) {
-	if (interaction.responded) return;
-	const focused = interaction.options.getFocused(true);
-	if (!focused) return;
-
-	const typed = (focused.value ?? '').toString().toLowerCase();
-
-	if (typed === '') {
-		const top5 = DataHandler.leaderboard.slice(0, 5).map(user => { 
-			return { name: user.ign ?? 'ERROR', value: user.ign ?? 'ERROR' }
-		});
-		return interaction.respond(top5);
-	}
-
-	const sortedNames = await DataHandler.getSortedNames();
-	const options = [{ name: typed, value: typed }];
-
-	// A rough sort that works for now
-	const matches = sortedNames.filter(name => name.toLowerCase().startsWith(typed)).slice(0, 9).map(name => {
-		return { name: name, value: name }
-	});
-
-	if (matches.length < 9) {
-		matches.push(...sortedNames.filter(name => name.toLowerCase().substring(1).startsWith(typed)).slice(0, 9 - matches.length).map(name => {
-			return { name: name, value: name }
-		}));
-	}
-	
-	if (matches.length <= 0) {
-		const top5 = DataHandler.leaderboard.slice(0, 5).map(user => { 
-			return { name: user.ign ?? 'ERROR', value: user.ign ?? 'ERROR' }
-		});
-		return interaction.respond([...options, ...top5]);
-	}
-
-	const total = [...options.filter(opt => opt.name.toLowerCase() !== matches[0]?.name.toLowerCase()), ...matches];
-
-	interaction.respond(total);
-}
-
 function GetCommand(name: string, type: CommandType): Command | undefined {
 	const command: Command | undefined = commands.get(name);
 
 	if (!command) return undefined;
 	// If type and command type are autocomplete it's valid
-	if (command.type === 'AUTOCOMPLETE' && type === 'AUTOCOMPLETE') return command; 
+	if (command.type === CommandType.Autocomplete && type === CommandType.Autocomplete) return command; 
 	// If the types don't match or the type isn't combo than it's invalid
-	if (![ 'COMBO', type ].includes(command.type)) return undefined;
+	if (command.type !== type || type !== CommandType.Combo) return undefined;
 
 	return command;
 }
@@ -100,10 +63,10 @@ async function HasPermsAndAccess(command: Command, interaction: CommandInteracti
 	if (!interaction.guildId || !command.permissions || !(interaction.member instanceof GuildMember)) return true;
 
 	if (command.adminRoleOverride) {
-		const server = await DataHandler.getServer(interaction.guildId);
+		const { data: server } = await FetchGuild(interaction.guildId).catch(() => ({ data: undefined }));
 
-		if (server && server.adminrole) {
-			const hasRole = HasRole(interaction.member, server.adminrole);
+		if (server && server.adminRole) {
+			const hasRole = HasRole(interaction.member, server.adminRole as unknown as string);
 
 			if (!hasRole) {
 				await interaction.reply({ 
