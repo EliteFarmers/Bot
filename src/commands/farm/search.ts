4b6f36cf2258667ec894cb46e5aaa63d917a8df1
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-06-04 11:48:09 -0700

    farm search command
    todo: button for each one that edits message to farm info component?

--- src/commands/farm/search.ts
+++ src/commands/farm/search.ts
@@ -1,7 +1,9 @@
 import { UserSettings } from 'api/elite.js';
 import { eliteCropOption } from 'autocomplete/crops.js';
 import { CommandAccess, CommandType, EliteCommand, SlashCommandOptionType } from 'classes/commands/index.js';
-import { AutocompleteInteraction, ChatInputCommandInteraction } from 'discord.js';
+import { EliteContainer } from 'classes/components.js';
+import { ChatInputCommandInteraction } from 'discord.js';
+import { farmsData, getCropFromName } from 'farming-weight';
 
 const command = new EliteCommand({
 	name: 'search',
@@ -17,4 +19,32 @@ const command = new EliteCommand({
 
 export default command;
 
-async function execute(interaction: ChatInputCommandInteraction, settings?: UserSettings) {}
+async function execute(interaction: ChatInputCommandInteraction, settings?: UserSettings) {
+	const cropName = interaction.options.getString('crop', false);
+	if (!cropName) return;
+	const crop = getCropFromName(cropName);
+	if (!crop) return;
+
+	const farms = Object.fromEntries(
+		Object.entries(farmsData)
+			.filter(([, farm]) => farm.crops.includes(crop))
+			.sort(([, a], [, b]) => b.bps - a.bps),
+	);
+
+	const component = new EliteContainer(settings).addTitle(`Farm designs for ${cropName}`);
+
+	Object.entries(farms).forEach(([, data], i) => {
+		if (i !== 1) {
+			component.addSeperator();
+		}
+
+		component.addDescription(`### ${data.name}`).addDescription(`bps: ${data.bps}`);
+	});
+
+	await interaction
+		.reply({
+			components: [component],
+			allowedMentions: { repliedUser: false },
+		})
+		.catch(() => undefined);
+}
